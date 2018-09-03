4ec8c60df4fd7f47cdee61a48246fd148e6b73f0
breeze7086 <breeze7086@gmail.com>
2018-09-03 10:39:36 +0800

    Change shell text color of okgreen & warning

--- truffleHog/truffleHog.py
+++ truffleHog/truffleHog.py
@@ -110,8 +110,8 @@ def get_strings_of_set(word, char_set, threshold=20):
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
+    OKGREEN = '\033[30m'
+    WARNING = '\033[31m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
     BOLD = '\033[1m'
