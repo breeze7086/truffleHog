7200ea654eb639ab5599e7f07adc2f2b7c6d5608
breeze7086 <breeze7086@gmail.com>
2019-01-16 13:31:20 +0800

    Update truffleHog.py

--- truffleHog/truffleHog.py
+++ truffleHog/truffleHog.py
@@ -197,11 +197,11 @@ def regex_check(printableDiff, commit_time, branch_name, prev_commit, blob, comm
     regex_matches = []
     for key in secret_regexes:
         found_strings = secret_regexes[key].findall(printableDiff)
-        if len(found_strings) >= 30:
-            found_strings = found_strings[0:30]
         for found_string in found_strings:
             found_diff = printableDiff.replace(printableDiff, bcolors.WARNING + found_string + bcolors.ENDC)
         if found_strings:
+            if len(found_strings) >= 30:
+                found_strings = found_strings[0:30]
             foundRegex = {}
             foundRegex['date'] = commit_time
             foundRegex['path'] = blob.b_path if blob.b_path else blob.a_path
